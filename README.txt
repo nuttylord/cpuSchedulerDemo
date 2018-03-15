456163fefb08b1c9d0112a1e0b3185908ed56bf3
nuttylord <lawless423@hotmail.com>
2018-03-15 11:06:59 +0000

    first push

---
+++ README.txt
@@ -0,0 +1,2 @@
+D00136306
+Tierna Lawless
---
+++ perlscript.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+sub random_integer {
+    my $min = shift;
+    my $max = shift;
+    return int(rand($max-$min)) + $min;
+}
+
+if (@ARGV < 3) {
+    die("usage: ca1_jobs.pl <jobs> <max arrival time> <max runtime>");
+}
+
+my ($jobs, $max_arrival, $max_runtime) = @ARGV;
+
+for ( my $job = 0 ; $job < $jobs ; ++$job ) {
+
+    my $arrival_time = random_integer(0, $max_arrival);
+    my $run_time = random_integer(1, $max_runtime);
+    
+    print "JOB_$job $arrival_time $run_time\n";
+}
